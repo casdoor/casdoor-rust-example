7f3e3cc1fd055f256311e8b067afa83a68e8ba97
qianxi0410 <894871277@qq.com>
2022-07-08 11:49:08 +0800

    feat: add demo code

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,158 @@
+// Copyright 2022 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![feature(decl_macro)]
 mod util;
 
-use casdoor_rust_sdk::{CasdoorConfig, CasdoorUser, UserService};
+use casdoor_rust_sdk::{AuthService, CasdoorConfig};
+use casdoor_rust_sdk::{CasdoorUser, UserService};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::response::Redirect;
+use rocket::serde::json::Json;
+use rocket::tokio::task;
+use rocket::{Request, Response};
+use util::abs_path;
 
-/// This is a sample code for casdoor-rust-sdk.
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // must pass a absolute path to the config file.
-    let conf = CasdoorConfig::from_toml(util::abs_path("conf.toml")?.as_str())?;
+#[macro_use]
+extern crate rocket;
+
+#[get("/login")]
+fn login() -> Json<String> {
+    let conf = CasdoorConfig::from_toml(abs_path("conf.toml").unwrap().as_str()).unwrap();
+    let auth_service = AuthService::new(&conf);
+
+    let redirect_url = auth_service.get_signin_url("http://localhost:8081/callback".to_string());
+    Json(redirect_url)
+}
+
+#[get("/signup")]
+fn signup() -> Json<String> {
+    let conf = CasdoorConfig::from_toml(abs_path("conf.toml").unwrap().as_str()).unwrap();
+    let auth_service = AuthService::new(&conf);
+
+    let redirect_url = auth_service.get_signup_url_enable_password();
+    Json(redirect_url)
+}
+
+#[get("/auth/<code>")]
+async fn callback(code: String) -> Json<CasdoorUser> {
+    let user = task::spawn_blocking(move || {
+        // perform the computation
+        let conf = CasdoorConfig::from_toml(abs_path("conf.toml").unwrap().as_str()).unwrap();
+        let auth_service = AuthService::new(&conf);
+
+        let token = auth_service.get_auth_token(code).unwrap();
+
+        let user = auth_service
+            .parse_jwt_token(token)
+            .expect("parse jwt token failed");
+
+        user
+    })
+    .await
+    .unwrap();
+
+    Json(user)
+}
+
+#[get("/logout")]
+fn logout() -> Redirect {
+    Redirect::to("/")
+}
+
+#[get("/user/count/<is_online>")]
+async fn user_count(is_online: String) -> Json<i64> {
+    let conf = CasdoorConfig::from_toml(abs_path("conf.toml").unwrap().as_str()).unwrap();
     let user_service = UserService::new(&conf);
 
-    // get user count.
-    // i have two users in my database.
-    // may you just one.
-    let count = user_service.get_user_count("0".to_string()).await?;
-    assert_eq!(count, 2);
+    let count = user_service.get_user_count(is_online).await.unwrap();
+    Json(count)
+}
 
-    // get user info.
-    let user = user_service.get_user("admin".to_string()).await?;
-    println!("{:#?}", user);
-    assert_eq!(user.display_name.to_lowercase(), "admin");
+#[get("/user/<name>")]
+async fn get_user(name: String) -> Json<CasdoorUser> {
+    let conf = CasdoorConfig::from_toml(abs_path("conf.toml").unwrap().as_str()).unwrap();
+    let user_service = UserService::new(&conf);
 
-    // create user.
-    let new_user = CasdoorUser {
-        name: "added_user".to_string(),
-        ..user
-    };
+    let user = user_service.get_user(name).await.unwrap();
+    Json(user)
+}
 
-    let code = user_service.add_user(new_user).await?;
-    assert_eq!(code, 200);
-    // get user count. should be 3.
-    let count = user_service.get_user_count("0".to_string()).await?;
-    assert_eq!(count, 3);
+#[get("/user/list")]
+async fn get_user_list() -> Json<Vec<CasdoorUser>> {
+    let conf = CasdoorConfig::from_toml(abs_path("conf.toml").unwrap().as_str()).unwrap();
+    let user_service = UserService::new(&conf);
 
-    // delete user.
-    let del_user = user_service.get_user("added_user".to_string()).await?;
-    let code = user_service.delete_user(del_user).await?;
-    assert_eq!(code, 200);
-    // get user count. should be 2.
-    let count = user_service.get_user_count("0".to_string()).await?;
-    assert_eq!(count, 2);
+    let users = user_service.get_users().await.unwrap();
+    Json(users)
+}
 
-    Ok(())
+#[post("/user/delete", data = "<user>")]
+async fn delete_user(user: Json<CasdoorUser>) -> Json<u16> {
+    let conf = CasdoorConfig::from_toml(abs_path("conf.toml").unwrap().as_str()).unwrap();
+    let user_service = UserService::new(&conf);
+
+    let code = user_service.delete_user(user.0).await.unwrap();
+    Json(code.as_u16())
+}
+
+#[post("/user/add", data = "<user>")]
+async fn add_user(user: Json<CasdoorUser>) -> Json<u16> {
+    let conf = CasdoorConfig::from_toml(abs_path("conf.toml").unwrap().as_str()).unwrap();
+    let user_service = UserService::new(&conf);
+
+    let code = user_service.add_user(user.0).await.unwrap();
+    Json(code.as_u16())
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().attach(Cors).mount(
+        "/api",
+        routes![
+            login,
+            signup,
+            callback,
+            logout,
+            user_count,
+            get_user,
+            get_user_list,
+            delete_user,
+            add_user,
+        ],
+    )
+}
+
+pub struct Cors;
+
+#[rocket::async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Fairing",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
 }
